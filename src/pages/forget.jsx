ded533277cc109c9a91f271847f158a9ab870dbe
nafiyanasrin <165750555+nafiyanasrin@users.noreply.github.com>
2024-04-05 16:47:35 +0530

    Add files via upload

---
+++ src/pages/forget.jsx
@@ -0,0 +1,10 @@
+import Forgotpage from '../components/forget';
+import Login from '../components/login';
+
+function ForgotPage() {
+  return (
+   <Forgot />
+  );
+}
+
+export default ForgotPage;
